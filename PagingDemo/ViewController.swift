2d72ff44239e1b753a987fb3a892d3615626a13c
estiqueahmed <rashel320@gmail.com>
2020-11-01 13:11:18 +0600

    adding missing file

--- PagingDemo/ViewController.swift
+++ PagingDemo/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource {
     
     var data = [String]()
-    
+    //
     let apiService = APIServic()
     
      private let tableview: UITableView = {
